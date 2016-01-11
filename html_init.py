a96b3e9acf659857aeda83097957b506e30b430d
Nikola Myslovskij <adriandecita@gmail.com>
2016-01-11 17:04:05 +0200

    new html project initializer

---
+++ html_init.py
@@ -0,0 +1,22 @@
+# This downloads empty blank project from gitHub.
+# It initializes your new project with it and creates
+# additional folders
+#
+# To call it from everywhere, simply move html_init and html_init.py
+# to /bin (on linux)
+import os
+import sys
+
+if (len(sys.argv) - 1):
+    output_folder = str(sys.argv[1])
+else:
+    output_folder = input("Project folder name: ")
+os.system("git clone https://github.com/AdriandeCita/blank_html_project.git " + output_folder)
+
+
+folders = [
+    "images",
+    "fonts"]
+
+for f in folders:
+    os.makedirs(output_folder + "/" + f)
