360092b0fd4c022b9ce1f4974379098a98c681cc
Nikola Myslovskij <adriandecita@gmail.com>
2016-01-13 01:11:50 +0200

    fix

--- css2css_parser.py
+++ css2css_parser.py
@@ -3,9 +3,53 @@
 # file in semi-minified syntax
 # Saves comments in code
 import tinycss
+import sys
 
-input_file = "style.css"
+input_file = "jquery-ui.css"
 output_file = "out.css"
+
+def preprocess(content):
+    ret = ""
+    while True:
+        bracket_counter = 0
+        block = ''
+        for x in content:
+            if x == "{":
+                bracket_counter += 1
+            elif x == "}":
+                bracket_counter -= 1
+            if bracket_counter == 0:
+                block = content[:content.index(x)] + x
+                if len(block):
+                    comments = []
+                    block_clean = ""
+                    while True:
+                        block_comment_begin = -1
+                        block_comment_end = -1
+                        block_comment_begin = block.find("/*")
+                        if block_comment_begin < 0:
+                            break
+                        block_comment_end = block.find("*/")
+                        if block_comment_end < 0:
+                            sys.exit("Preprocessing problem in block {" + block + "}")
+                        comments.append(block[block_comment_begin:block_comment_end+2])
+                        block_clean += block[:block_comment_begin]
+                        block = block[block_comment_end+2:]
+                    block_clean += block
+                    for c in comments:
+                        block_clean += c
+                    if len(block_clean):
+                        ret += block_clean
+                    else:
+                        ret += block
+                if len(content) > 1:
+                    content = content[content.index(x)+1:]
+                else:
+                    content = ""
+                break
+        if len(content) == 0:
+            break
+    return(ret)
 
 def rule_to_css(rule):
     str_out = ""
@@ -43,6 +87,7 @@ for line in stream_in:
     content += line
 stream_in.close()
 
+content = preprocess(content)
 while True:
     index = -1
     index = content.find("/*")
@@ -54,8 +99,8 @@ while True:
 
         index = content.find("*/")
         if index > -1:
-            rebuilt_stylesheet += content[:index+1]
-            content = content[index+1:]
+            rebuilt_stylesheet += content[:index+2]
+            content = content[index+2:]
         else:
             rebuilt_stylesheet += content
             content = ""
